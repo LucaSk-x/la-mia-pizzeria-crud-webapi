86ebe6ec3ff4561f7d1353270714b4b13686448c
LucaSk-x <lucapiccinno64@gmail.com>
2022-11-29 16:32:25 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/Api/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using la_mia_pizzeria_static.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        IDbPizzaRepository _pizzaRepository;
+        public PizzaController()
+        {
+            _pizzaRepository = new DbPizzaRepository();
+        }
+        public IActionResult Get()
+
+        {
+            return Ok(_pizzaRepository.Get());
+        }
+
+        public IActionResult Search(string title)
+        {
+            return Ok(_pizzaRepository.SearchByTitle(title));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Details(int id)
+        {
+            return Ok(_pizzaRepository.Get(id));
+        }
+    }
+}
---
+++ Models/Repositories/IDbPizzaRepository.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_static.Models.Form;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Models.Repositories
+{
+    public interface IDbPizzaRepository
+    {
+        void Create(Pizza pizza, List<int> selectedIngredients);
+        PizzaForm CreateForm();
+        PizzaForm CreateForm(int id);
+        PizzaForm CreateForm(PizzaForm formData);
+        void Delete(Pizza pizza);
+        bool Exists(int id);
+        List<Pizza> Get();
+        List<Pizza> Get(bool relation);
+        Pizza Get(int id);
+        List<Pizza> GetByCategoryId(int categoryId);
+        List<Pizza> SearchByTitle(string title);
+        void Update(Pizza updatedPizza, List<int> selectedIngredients);
+    }
+}
