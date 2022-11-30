2f09753741dc5175184e46ac3fd5d0130f6795bf
LucaSk-x <lucapiccinno64@gmail.com>
2022-11-30 15:05:21 +0100

    Creato MessageController con funzioni relative

---
+++ Controllers/Api/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using la_mia_pizzeria_crud_webapi.Models;
+using la_mia_pizzeria_static.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_webapi.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private PizzaDbContext db;
+        public MessageController(PizzaDbContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Message message)
+        {
+            try
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                return UnprocessableEntity(e.Message);
+            }
+            return Ok(message);
+        }
+    }
+}
